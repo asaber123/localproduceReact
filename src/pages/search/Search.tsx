3da1d50cf69b1977ee5a6138b8e696f40c862c0c
Åsa Berglund <71882921+asaber123@users.noreply.github.com>
2022-03-09 09:47:25 +0100

    get  and mapping works

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,22 +1,47 @@
-import * as React from "react";
-import {useHistory} from "react-router-dom";
+///import {useHistory} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
-export function Search () {
-    const getProduce = async () => {
-        const response = await fetch(
-          "https://localhost:7247/api/apiproduce",{
-          method: "GET",
-          headers: {'Content-Type': 'application/json'}
-        });
-        const data = await response.json();
-      };
-      console.log(getProduce());
 
-    return(
+export function Search() {
+    const [data, setData] = React.useState<any[]>([])
+    React.useEffect(() => {
+        const getProduce = async () => {
+            await fetch("https://localhost:7247/api/apiproduce")
+                .then((res) => res.json())
+                .then((data) => {
+                    setData(data);
+                    console.log(data)
+                })
+        };
+        getProduce();
+    }, []);
+
+
+
+    // const getProduce = async () => {
+    //     const response = await fetch(
+    //       "https://localhost:7247/api/apiproduce",{
+    //       method: "GET",
+    //       headers: {'Content-Type': 'application/json'}
+    //     });
+    //     const data = await response.json();
+    //   };
+    //   console.log(getProduce());
+
+    const arr = data.reverse().map((produce => {
+        return (
+            <div>
+
+                <p>name: {produce.produceName}</p>
+            </div>
+        );
+    }));
+
+    return (
         <div>
-
-            
+        {arr}
         </div>
     );
 
 }
+
