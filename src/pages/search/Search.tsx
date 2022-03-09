aee1a3892d81e9b06e42cf638854e649130be51d
Åsa Berglund <71882921+asaber123@users.noreply.github.com>
2022-03-09 15:36:30 +0100

    search functions workrs

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 export function Search() {
     const [data, setData] = React.useState<any[]>([])
+    const [producer, setProducer] = React.useState<any[]>([])
     React.useEffect(() => {
         const getProduce = async () => {
             await fetch("https://localhost:7247/api/apiproduce")
@@ -15,31 +16,78 @@ export function Search() {
         };
         getProduce();
     }, []);
+    React.useEffect(() => {
+        const getProducer = async () => {
+            await fetch("https://localhost:7247/api/apiproducer")
+                .then((res) => res.json())
+                .then((producer) => {
+                    setProducer(producer);
+                    console.log(producer)
+                })
+        };
+        getProducer();
+    }, []);
 
+    const [isSearch, setSearch] = useState("");
+    const [isSearchPickup, setPickupSeatch] = useState("");
+    const [isSearchProducer, setSearchProducer] = useState("");
+    
+    const arr = data.reverse().filter((q) => {
+        if (isSearch === "") {
+          return q;
+        } else if (q.produceName.toLowerCase().includes(isSearch.toLowerCase())) {
+          return q;
+        } else {
+          return "";
+        }
+      }).filter((q) => {
+        if (isSearchPickup === "") {
+          return q;
+        } else if (q.pickupPlace.toLowerCase().includes(isSearchPickup.toLowerCase())) {
+          return q;
+        } else {
+          return "";
+        }
+      }).filter((q) => {
+        if (isSearchProducer === "") {
+          return q;
+        } else if (q.producerId.toString().includes(isSearchProducer)) {
+          return q;
+        } else {
+          return "";
+        }
+      }).map((produce => {
+        let item = "";
+        for (let i = 0; i < producer.length; i++) {
+            if (produce.producerId == producer[i].producerId) {
 
+                item = producer[i].producerName;
+            }
+        }
 
-    // const getProduce = async () => {
-    //     const response = await fetch(
-    //       "https://localhost:7247/api/apiproduce",{
-    //       method: "GET",
-    //       headers: {'Content-Type': 'application/json'}
-    //     });
-    //     const data = await response.json();
-    //   };
-    //   console.log(getProduce());
-
-    const arr = data.reverse().map((produce => {
         return (
-            <div>
 
-                <p>name: {produce.produceName}</p>
+            <div className="produce-div">
+                <h3>{produce.produceName}</h3>
+                <p><b>Producer:</b> {item}</p>
+                <p><b>Price:</b> {produce.price}</p>
+                <p><b>Pickup Place: </b>{produce.pickupPlace}</p>
+                <p><b>Theme: </b>{produce.theme}</p>
+                <p><b>Description:</b> {produce.description}</p>
             </div>
         );
     }));
 
     return (
-        <div>
-        {arr}
+        <div className="search">
+            <input type="text" onChange={(e)=>{setSearch(e.target.value)}} />
+            <select onChange={(e)=>{setSearchProducer(e.target.value)}}> <option> </option>{producer.map(e =>{
+               return(
+
+                 <option value={e.producerId}> {e.producerName} </option>)}
+            )
+            } </select> 
+            {arr}
         </div>
     );
 
